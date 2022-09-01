1a21d37b34d59a64b95e2ab3e4b52581ef41afd8
Sergio Berdeja <sergioberdeja@gmail.com>
2022-09-01 15:43:36 -0600

    Completed assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::StatementInvalid with: :creation_error
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json:user
+    end
+
+    def show 
+        user = User.find(session[:user_id])
+        if user 
+            render json:user 
+        else
+            render json:{error:'Not logged in'}, status: :unauthorized
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def creation_error 
+        render json:{errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+end
